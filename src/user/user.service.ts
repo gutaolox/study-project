76a21122e0e68fb6d491392c2ce8876fc79a154e
Gustavo Motta Tranche <32674347+gutaolox@users.noreply.github.com>
2022-06-02 11:24:57 -0300

    initial commit simple request

---
+++ src/user/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const newUser = { ...createUserDto } as User;
+
+    return await this.usersRepository.save(newUser);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOne({ where: { id: id } });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const updateEntity = await this.findOne(id);
+    updateEntity.username = updateUserDto.username;
+    this.usersRepository.save(updateEntity);
+  }
+}
